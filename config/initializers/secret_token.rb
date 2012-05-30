fab0580bb04caf77bf2992ed815ec9736f4ddfdc
Sayanee <sayanee@gmail.com>
2012-05-30 19:06:59 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fatcat::Application.config.secret_token = '4d540e9bc54faac41f82ec8837f03cc3ba9c4ebcd7e76b31828551d0fc24d8571e00e0cd9056a2add2ef7591d3692e630ba9eafa737f4566025d3cbd00bdd591'
